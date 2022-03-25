b163aa665a7b705a88db9c0d1a64413cce3fdbf7
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-03-25 18:30:45 +0700

    style

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -2,35 +2,25 @@
 	import { page } from '$app/stores';
 </script>
 
-<div class="flex justify-left ml-[16px] mt-[16px]">
-	<img src="https://upload.wikimedia.org/wikipedia/vi/c/c9/Logo_Thpt_Gia_Dinh.gif" alt="GiaDinh" 
-		width="110"
-		height="110"
-	/>
-	<div class="title">
-		<b>GDI LIBRARY</b>
+<header>
+	<div class="flex flex-row m-4 items-center">
+		<img src="https://upload.wikimedia.org/wikipedia/vi/c/c9/Logo_Thpt_Gia_Dinh.gif" alt="GiaDinh" width="110" height="110"/>
+		<div class="title relative text-center text-6xl w-full">
+			<b>GDI LIBRARY</b>
+		</div>
 	</div>
-</div>
-<!-- <div class="title">
-	<b>GDI LIBRARY</b>
-</div> -->
-
-<header class="flex flex-col sm:flex-row items-start">
-	<!-- <img src="https://upload.wikimedia.org/wikipedia/vi/c/c9/Logo_Thpt_Gia_Dinh.gif" alt="GiaDinh" width="119"/> -->
-	<div class="bg-neutral-700 py-2 m-4 w-full">
-		<ul class="flex flex-col sm:flex-row items-center gap-2 justify-evenly text-neutral-300">
-			<li class:active={$page.url.pathname === '/'}>
-				<a sveltekit:prefetch href="/">HOMEPAGE</a>
-			</li>
-			<li class:active={$page.url.pathname === '/problems'}>
-				<a sveltekit:prefetch href="/problems">PROBLEMS</a>
-			</li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">GIỚI THIỆU</a>
-			</li>
-			<li class:active={$page.url.pathname === '/contact'}>
-				<a sveltekit:prefetch href="/contact">LIÊN HỆ</a>
-			</li>
-		</ul>
+	<div class="bg-neutral-700 py-2 w-full sm:h-16 shadow flex flex-col sm:flex-row justify-evenly items-center gap-2">
+		<div class:active={$page.url.pathname === '/'}>
+			<a sveltekit:prefetch href="/">HOMEPAGE</a>
+		</div>
+		<div class:active={$page.url.pathname === '/problems'}>
+			<a sveltekit:prefetch href="/problems">PROBLEMS</a>
+		</div>
+		<div class:active={$page.url.pathname === '/about'}>
+			<a sveltekit:prefetch href="/about">GIỚI THIỆU</a>
+		</div>
+		<div class:active={$page.url.pathname === '/contact'}>
+			<a sveltekit:prefetch href="/contact">LIÊN HỆ</a>
+		</div>
 	</div>
 </header>
