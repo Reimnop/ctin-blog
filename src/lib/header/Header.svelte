1ddf68117fc386b18c204b97d82b77b5b192c0f7
long89 <imtheboss892005@gmail.com>
2022-03-25 17:39:09 +0700

    logo
    fix logo

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -2,14 +2,20 @@
 	import { page } from '$app/stores';
 </script>
 
+<div class="flex justify-left">
+	<img src="https://filethpt.hcm.edu.vn//UploadImages/thptgiadinh/logo/Logo-GiaDinh.jpg" alt="GiaDinh" 
+		width="119"
+	/>
+</div>
+
 <header>
 	<div class="bg-neutral-700 py-2 m-4">
 		<ul class="flex flex-col sm:flex-row items-center gap-2 justify-evenly text-neutral-300">
 			<li class:active={$page.url.pathname === '/'}>
-				<a sveltekit:prefetch href="/">TRANG CHỦ</a>
+				<a sveltekit:prefetch href="/">HOMEPAGE</a>
 			</li>
-			<li class:active={$page.url.pathname === '/classwork'}>
-				<a sveltekit:prefetch href="/classwork">BÀI TẬP</a>
+			<li class:active={$page.url.pathname === '/problems'}>
+				<a sveltekit:prefetch href="/problems">PROBLEMS</a>
 			</li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="/about">GIỚI THIỆU</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,3 @@
 	<title>Home</title>
 </svelte:head>
 
-<div class="flex justify-center">
-	<img src="https://filethpt.hcm.edu.vn//UploadImages/thptgiadinh/logo/Logo-GiaDinh.jpg" alt="GiaDinh" />
-</div>
