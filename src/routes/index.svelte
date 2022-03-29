7dcea791efb54f858bc9bbd4c7c3d1bccc669adc
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-03-30 00:01:33 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,9 +9,9 @@
 	<link rel="icon" type="image/png" href="logo_gd.png" />
 </svelte:head>
 
-<div class="flex flex-row flex-wrap gap-4">
+<div class="flex flex-row flex-wrap">
 	{#each Array(20) as _}
-		<div class="w-96">
+		<div class="basis-1/3 p-2">
 			<Card>
 				<b slot="header">Hello world!</b>
 				<span slot="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac nisl ut quam tincidunt lobortis a sit amet lectus. Nunc vel libero pretium erat auctor condimentum quis pellentesque lectus. Vestibulum sem nisl, tempor non sapien sit amet, ultricies maximus quam. Maecenas mollis auctor libero a dictum. Morbi tempor velit quis leo consectetur, et mollis purus fringilla. Pellentesque rhoncus odio a dui ultricies laoreet. Nam convallis porta consectetur. Nulla at risus viverra ligula bibendum dapibus. Nam finibus a nisl quis feugiat. Praesent eleifend quam eu congue dignissim. Etiam consequat quis magna sit amet dignissim. Nam non rutrum est. Sed tempus erat commodo diam porta, ut eleifend urna mollis. Quisque elit eros, tincidunt id tellus vitae, ultrices fermentum tellus. Sed porta consectetur turpis, vel efficitur augue semper non. Aliquam elit est, porttitor sed velit sit amet, tempus rutrum leo.</span>
